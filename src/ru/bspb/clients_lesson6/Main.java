061b3b968e4ac9d2dd6872db231bb7b0aae309cc
marinademesh <marinademesh@gmail.com>
2021-03-14 23:26:13 +0300

    1

--- src/ru/bspb/clients_lesson6/Main.java
+++ src/ru/bspb/clients_lesson6/Main.java
@@ -20,37 +20,50 @@ public class Main {
         pl.add(new Person("Кусков Рафинад Сахарович", 88, Gender.MALE));
         pl.add(new Person("Шариков Шарик Шарикович", 27, Gender.MALE));
         pl.add(new Person("Бубликов Петр Сергеевич", 32, Gender.MALE));
-        Collections.sort(pl);// Сортируем по возрастанию предварительно в классе Person мы переопределили compare: public int compareTo(Person o) {return this.getAge()-o.getAge(); }
-
-        //  ** Проверяем вывод, что Клиенты отсортированы по возрастанию:
-        //  Iterator <Person> iterator = pl.iterator();
-        //  while (iterator.hasNext()) {
-        //     Person tmp = iterator.next();
-        //     System.out.println(tmp);
-        // }
 
         Set<Person> pl1 = new HashSet<>(); // Записываем Arraylist в Set, чтобы удалить дубликаты
         pl1.addAll(pl);
 
         // ** Проверяем, что при записи в Set удалились дубликаты
-        //   Iterator <Person> iterator = pl1.iterator();
-        //  while (iterator.hasNext()) {
-        //     Person tmp1 = iterator.next();
-        //   System.out.println(tmp1);
-        //  }
+        //  Iterator <Person> iterator = pl1.iterator();
+        //   while (iterator.hasNext()) {
+        //    Person tmp1 = iterator.next();
+        //    System.out.println(tmp1);
+        //     }
 
-        ArrayList<Person> pl2 = new ArrayList<>(); //Записываем Клиентов обратнов в Arraylist
+        ArrayList<Person> pl2 = new ArrayList<>(); //Записываем Клиентов обратно в в Arraylist
         pl2.addAll(pl1);
 
-        ///Удаляем всех женщин и печатаем мужчин
-          Iterator <Person> iterator = pl2.iterator();
-          while (iterator.hasNext()) {
-             Person tmp2= iterator.next();
-            if (tmp2.getGender() == Gender.FEMALE) {
-              pl2.remove(tmp2);
-          }
-                else System.out.println(tmp2);
+        Collections.sort(pl2);// Сортируем по возрастанию предварительно в классе Person мы переопределили compare: public int compareTo(Person o) {return this.getAge()-o.getAge(); }
+
+        //  ** Проверяем вывод, что Клиенты отсортированы по возрастанию:
+        //    Iterator <Person> iterator = pl2.iterator();
+        //    while (iterator.hasNext()) {
+        //       Person tmp2 = iterator.next();
+        //       System.out.println(tmp2);
+        //     }
+
+        ArrayList<Person> pl3 = new ArrayList<>();
+        pl3.addAll(pl2);
+
+        // **Удаляем всех женщин и печатаем мужчин
+        Iterator<Person> iterator = pl3.iterator();
+        while (iterator.hasNext()) {
+            Person tmp3 = iterator.next();
+            if (tmp3.getGender() == Gender.FEMALE) {
+                iterator.remove();
+            }
+        }
+        //** Для красивого вывода
+       ArrayList<Person> result = new ArrayList<>();
+       result.addAll(pl3);
+        Iterator <Person> iterator1 = result.iterator();
+        while (iterator1.hasNext()) {
+            Person bmp = iterator1.next();
+             System.out.println(bmp);
            }
-     //    System.out.println(pl2); //Почему, если мы распечатываем просто весь список без else - неправильный итог, есть женщина?
+     // System.out.println(pl3);
     }
 }
+
+
